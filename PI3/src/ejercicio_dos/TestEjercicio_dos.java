442bf1765baf3fed153758b555bb3d6cc860fd07
Enrique Reina <Enrique Reina@SI-LO-LEES-FUISTE-HACKEADO>
2019-03-25 18:49:43 +0100

    0.0.1

--- PI3/src/ejercicio_dos/TestEjercicio_dos.java
+++ PI3/src/ejercicio_dos/TestEjercicio_dos.java
@@ -15,26 +15,15 @@ public class TestEjercicio_dos {
 	public static void main(String[] args) {
 
 		Graph<Monumento, Ruta> grafoConexiones = Ejercicio_dos.createGraph("ficheros/problema_cuatro/P4-Grafo1.txt");
-		Graph<Monumento, Ruta> grafoPrecedencias = Ejercicio_dos
-				.createDirGraph("ficheros/problema_cuatro/P4-Grafo2.txt");
+		Graph<Monumento, Ruta> grafoPrecedencias = Ejercicio_dos.createDirGraph("ficheros/problema_cuatro/P4-Grafo2.txt");
 
 		System.out.println("=====================APARTADO A========================");
-
-		List<Set<Monumento>> s = new ArrayList<>();
-		s = Ejercicio_dos.componentesConexas(grafoConexiones);
-		System.out.println("Estan todos los caminos conectados entre si: " + Ejercicio_dos.estaConectado(s));
-		System.out.println("Las componentes conexas son: ");
-		for (Set<Monumento> l : s) {
-			System.out.println(l);
-		}
+		List<Set<Monumento>> s = Ejercicio_dos.componentesConexas(grafoConexiones);
+		imprimeA(s);
 
 		System.out.println("\n=====================APARTADO B========================");
-
-		System.out.println("Sitios que pueden visitarse sin haber visitado otros anteriormente: ");
-		for (Monumento m : Ejercicio_dos.apartadoB(grafoPrecedencias)) {
-			System.out.println(m);
-		}
-
+		imprimeB(grafoPrecedencias);
+		
 		System.out.println("\n=====================APARTADO C========================");
 
 		List<Monumento> grupo1 = Arrays.asList(Monumento.create("Sitio5"), Monumento.create("Sitio7"),
@@ -45,10 +34,31 @@ public class TestEjercicio_dos {
 				Monumento.create("Sitio2"));
 
 		List<List<Monumento>> sitios = Arrays.asList(grupo1, grupo2, grupo3);
+		imprimeC(sitios, grafoConexiones, grafoPrecedencias);
 
-		for (List<Monumento> l : sitios)
-			System.out.println(Ejercicio_dos.apartadoC(l, grafoConexiones, grafoPrecedencias));
-
+	}
+	
+	public static void imprimeA(List<Set<Monumento>> s)
+	{
+		System.out.println("Estan todos los caminos conectados entre si: " + Ejercicio_dos.estaConectado(s));
+		System.out.println("Las componentes conexas son: ");
+		for (Set<Monumento> l : s) {
+			System.out.println(l);
+		}
+	}
+	
+	public static void imprimeB(Graph<Monumento, Ruta> grafoPrecedencias)
+	{
+		System.out.println("Sitios que pueden visitarse sin haber visitado otros anteriormente: ");
+		for (Monumento m : Ejercicio_dos.apartadoB(grafoPrecedencias)) {
+			System.out.println(m);
+		}
+	}
+	
+	public static void imprimeC(List<List<Monumento>> sitios,Graph<Monumento, Ruta> grafoConexiones,Graph<Monumento, Ruta> grafoPrecedencias)
+	{
+			for (List<Monumento> l : sitios)
+				System.out.println(Ejercicio_dos.apartadoC(l, grafoConexiones, grafoPrecedencias));
 	}
 
 }
--- PI3/src/ejercicio_uno/TestAlgoritmo.java
+++ PI3/src/ejercicio_uno/TestAlgoritmo.java
@@ -18,6 +18,25 @@ public class TestAlgoritmo {
 		String grupoVecinos = "{b0, b1}, {b0, b1, b5}, {b2, b3}, {b2, b3, b4}, {b3, b4, b5}, {b1, b4, b5}";
 		Integer n = 6;
 		
+		ImprimeSolucion(n, grupoVecinos);
+		
+	}
+	
+	public static void setCondiciones()
+	{
+		AlgoritmoAG.ELITISM_RATE = 0.30;
+		AlgoritmoAG.CROSSOVER_RATE = 0.8;
+		AlgoritmoAG.MUTATION_RATE = 0.7;
+		AlgoritmoAG.POPULATION_SIZE = 50;
+
+		StoppingConditionFactory.NUM_GENERATIONS = 50;
+		StoppingConditionFactory.SOLUTIONS_NUMBER_MIN = 1;
+		StoppingConditionFactory.FITNESS_MIN = 1;
+		StoppingConditionFactory.stoppingConditionType = StoppingConditionType.SolutionsNumber;
+	}
+	
+	public static void ImprimeSolucion(Integer n, String grupoVecinos)
+	{
 		ValuesInRangeProblemAG<Integer, List<Integer>> p = new Algoritmo(n,grupoVecinos);
 		AlgoritmoAG<BinaryChromosome> alg = AlgoritmoAG.create(ChromosomeType.Binary, p);
 		alg.ejecuta();
@@ -35,18 +54,4 @@ public class TestAlgoritmo {
 			
 		System.out.println("Coste (Numero de esatciones): " + -1 *  p.fitnessFunction(sol));
 	}
-	
-	public static void setCondiciones()
-	{
-		AlgoritmoAG.ELITISM_RATE = 0.30;
-		AlgoritmoAG.CROSSOVER_RATE = 0.8;
-		AlgoritmoAG.MUTATION_RATE = 0.7;
-		AlgoritmoAG.POPULATION_SIZE = 50;
-
-		StoppingConditionFactory.NUM_GENERATIONS = 50;
-		StoppingConditionFactory.SOLUTIONS_NUMBER_MIN = 1;
-		StoppingConditionFactory.FITNESS_MIN = 1;
-		StoppingConditionFactory.stoppingConditionType = StoppingConditionType.SolutionsNumber;
-	}
-
 }
